4e079e2c04af95636571aa6b009d289449307882
Michal <mkudr@synopsis.cz>
2018-02-18 14:23:26 +0100

    initial commit

--- src/Logger.php
+++ src/Logger.php
@@ -10,15 +10,18 @@ use Monolog;
  */
 class Logger extends Monolog\Logger
 {
-	public function __construct($name, $handlers = [], $processors = []) {
+	public function __construct($name, $handlers = [], $processors = [])
+	{
 		parent::__construct($name, $handlers, $processors);
 	}
 
-	public function addRecord(int $level, $message, array $context = array()) {
+	public function addRecord(int $level, $message, array $context = [])
+	{
 		if ($message instanceof \Exception) {
 			$context['exception'] = $message;
 		}
 		$context['level'] = $level;
+
 		return parent::addRecord($level, $message, $context);
 	}
 }
